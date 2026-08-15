219c58435492715df9c09b59f79647474291ec37
GCP <7726348465g@gmail.com>
2026-08-15 03:04:29 +0100

    settings for pythonanywhere deploy (sqlite)

--- phish_project/settings.py
+++ phish_project/settings.py
@@ -75,13 +75,8 @@ WSGI_APPLICATION = "phish_project.wsgi.application"
 if "PYTHONANYWHERE_DOMAIN" in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'phish_db',
-            'USER': 'admin',
-            'PASSWORD': 'AEAEAEEOO',
-            
-            'HOST': 'LeavesFallFallFall.mysql.pythonanywhere-services.com',
-            'PORT': '3306',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 else:
